ca0e72502c2f062c3e149cdb1b8ee1f33d315091
Hakan Baba <ahakanbaba@gmail.com>
2018-12-02 21:07:59 -0800

    Add a better error message for unrecognizable remotes. Fixes #25

--- test/run.sh
+++ test/run.sh
@@ -10,8 +10,10 @@ cd `dirname "$0"`
 #
 # The `-e -s` and `2>&1` trickery is required for the output to be displayed
 # correctly in environments without interactive tty, such as Travis CI.
+#
+# Attempt to edit a file called testFile. Some tests expect a file parameter to vim.
 vim -n -e -s -N -U NONE -i NONE -u <(cat << VIMRC
 set rtp+=../
 source ./vim-gh-line_test.vim
-VIMRC) -c 'RunAllTests' 2>&1
+VIMRC) -c 'RunAllTests' testFile 2>&1
 
